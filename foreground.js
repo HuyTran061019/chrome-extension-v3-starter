cfe5299a2201d40ae37793b64d2bdce5489e661e
HuyTran1412 <57608002+HuyTran061019@users.noreply.github.com>
2023-02-17 11:02:42 +0700

    Extension test 1

--- foreground.js
+++ foreground.js
@@ -1,7 +1,36 @@
+/*global chrome*/
+
 // This script gets injected into any opened page
 // whose URL matches the pattern defined in the manifest
 // (see "content_script" key).
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+console.log("Hello linkedin from Huy 1234");
+
+const getToken = async () => {
+  const icon = document.createElement("img");
+  icon.src = chrome.runtime.getURL("logo/logo-48.png");
+  icon.style.cssText =
+    "border-radius: 10px; padding: 8px; border: 1px solid #40B6E6; box-shadow: 0 3px 10px rgb(0 0 0 / 45%);";
+
+  const button = document.createElement("button");
+  button.appendChild(icon)
+  button.style.cssText = "position: fixed; top: 80px; right: 30px;  cursor: pointer; z-index: 999"
+  button.addEventListener("click", function () {
+    alert("Get token for Talent-e");
+    chrome.runtime.sendMessage(
+      {
+        message: "messageSent",
+      },
+      function (response) {
+        console.log("Token: ", response);
+      }
+    );
+  });
+  document.body.appendChild(button);
+
+
+};
+
+getToken();
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,38 @@
+/*global chrome*/
+
 // This is the service worker script, which executes in its own context
 // when the extension is installed or refreshed (or when you access its console).
 // It would correspond to the background script in chrome extensions v2.
-
-console.log("This prints to the console of the service worker (background script)")
-
 // Importing and using functionality from external files is also possible.
-importScripts('service-worker-utils.js')
 
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
 // The path should be relative to the file `manifest.json`.
+chrome.runtime.onMessage.addListener(
+  // this is the message listener
+  function (request, sender, sendResponse) {
+    if (request.message === "messageSent") console.log("SENT message");
+    chrome.cookies.getAll(
+      {
+        domain: ".linkedin.com",
+      },
+      function (cookies) {
+        let csrf_token =''
+        // console.log("Here1", cookies)
+        const reduced = cookies.reduce((carry, item, index) => {
+            if(item.name === "JSESSIONID"){
+                csrf_token=item.value
+            }
+            carry[index] = `${item.name}=${item.value}`
+            return carry;
+        }, {});
+        // console.log("Here1 ---- reduced = " , reduced)
+        let result = Object.values(reduced).join("; ")
+        console.log('linkedin token: ', result)
+        console.log('csrf_token: ', csrf_token)
+      }
+    );
+  }
+);
+
+importScripts("service-worker-utils.js");
